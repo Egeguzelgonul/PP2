914cf9702324851b74cd67a686b23b722c6b724c
unknown <eguzelgonul@gmail.com>
2019-02-09 09:43:01 +0600

    comment

--- week1/task4/task4/task4/Program.cs
+++ week1/task4/task4/task4/Program.cs
@@ -13,13 +13,13 @@ namespace testing_stuff
 
             int n = Convert.ToInt32(Console.ReadLine()); //declaring an integer and inputting it in the console
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) //cycle for the number of rows in total
             {
-                for (int j = -1; j < i; j++)
+                for (int j = -1; j < i; j++) //second cycle which always repeats itself as much as there is current "i", example: i = 3, 3 = number of stars printed
                 {
-                    Console.Write("[*]");
+                    Console.Write("[*]"); //simply printing one star
                 }
-                Console.Write(System.Environment.NewLine);
+                Console.Write(System.Environment.NewLine); //it's like endl; in c++, presses enter and jumps to the next row
             }
 
             // Variable
