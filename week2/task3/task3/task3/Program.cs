c9335ea74a7d7364e505e4fc33e9ccc83792293f
unknown <eguzelgonul@gmail.com>
2019-02-11 21:08:13 +0600

    comment

--- week2/task3/task3/task3/Program.cs
+++ week2/task3/task3/task3/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Windows.Forms;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,26 +9,25 @@ namespace task3
 {
     class Program
     {
-        private void ListDirectory(TreeView treeView, string path)
+        static void KBTU_RECURSIVE(string directory, int level)
         {
-            treeView.Nodes.Clear();
-            var rootDirectoryInfo = new DirectoryInfo(path);
-            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
-        }
+            if (!Directory.Exists(directory))
+                return;
+            string interval = new string('\t', level++);
+            string[] files = Directory.GetFiles(directory);
 
-        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
-        {
-            var directoryNode = new TreeNode(directoryInfo.Name);
-            foreach (var directory in directoryInfo.GetDirectories())
-                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
-            foreach (var file in directoryInfo.GetFiles())
-                directoryNode.Nodes.Add(new TreeNode(file.Name));
-            return directoryNode;
+            Console.WriteLine(string.Concat(interval, " ", directory));
+
+            interval += "\t";
+            foreach (string file in files)
+                Console.WriteLine(string.Concat(interval, " ", Path.GetFileName(file)));
+            foreach (string fold in Directory.GetDirectories(directory))
+                KBTU_RECURSIVE(fold, level);
         }
 
         static void Main(string[] args)
         {
-
+            KBTU_RECURSIVE(@"C:\Users\asus\Desktop\Üni\PP2\", 0);
         }
     }
 }
